ef095755e91de91212f49e379a7ed3b963c0db7a
VasiliyZaharov <zaharovfreelance@gmail.com>
2023-02-17 20:24:29 +0300

    add views.py + add urls.py

--- fan_board/project/service/views.py
+++ fan_board/project/service/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Article
+from .filters import ArticleFilter
+from .forms import ArticleForm
+
+class ArticleList(ListView):
+    model = Article
+    ordering = '-id'
+    template_name = 'article.html'
+    context_object_name = 'article'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ArticleFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class ArticleDetail(DetailView):
+    model = Article
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class ArticleCreate(CreateView):
+    form_class = ArticleForm
+    model = Article
+    template_name = 'article_edit.html'
+
+
+class ArticleUpdate(UpdateView):
+    form_class = ArticleForm
+    model = Article
+    template_name = 'article_edit.html'
+
+class ArticleDelete(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article')
