699f7c858f00848655530e64274f0f5a5ba96356
VasiliyZaharov <zaharovfreelance@gmail.com>
2023-02-17 20:32:25 +0300

    add project\urls.py + add service in settings.py

--- fan_board/project/project/settings.py
+++ fan_board/project/project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.flatpages',
 
     'fpages',
+    'service',
 ]
 
 SITE_ID = 1
--- fan_board/project/project/urls.py
+++ fan_board/project/project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
+    path('article/', include('service.urls')),
 ]
