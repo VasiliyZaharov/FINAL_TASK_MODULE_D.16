5d47439cc637b29c0862e1044cec39b92b8bba52
VasiliyZaharov <zaharovfreelance@gmail.com>
2023-02-22 19:58:13 +0300

    finalized .env and settings.py

--- fan_board/project/project/urls.py
+++ fan_board/project/project/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("allauth.urls")),
+    path('accounts/', include('allauth.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('article/', include('service.urls')),
 ]
